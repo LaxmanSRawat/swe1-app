5150e4b7c2ec02ba91647d0fcbebcac02c07d2f6
Laxman Singh Rawat <94244310+LaxmanSRawat@users.noreply.github.com>
2026-03-06 23:28:42 -0500

    Add .travis.yml

---
+++ README.md
@@ -0,0 +1,14 @@
+# SWE1 Django Polls App
+
+[![Build Status](https://app.travis-ci.com/LaxmanSRawat/swe1-app.svg?branch=main)](https://app.travis-ci.com/github/LaxmanSRawat/swe1-app)
+[![Coverage Status](https://coveralls.io/repos/github/LaxmanSRawat/swe1-app/badge.svg?branch=main)](https://coveralls.io/github/LaxmanSRawat/swe1-app?branch=main)
+
+## CI/CD
+
+- Travis CI runs on push and pull requests.
+- CI checks include:
+  - `black --check`
+  - `flake8`
+  - Django tests with `coverage`
+  - coverage upload with `coveralls`
+- On successful CI, Travis deploys to AWS Elastic Beanstalk (main branch).
--- djangotutorial/polls/tests.py
+++ djangotutorial/polls/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import Question
+
+
+def create_question(question_text, days_offset):
+    return Question.objects.create(
+        question_text=question_text,
+        pub_date=timezone.now() + datetime.timedelta(days=days_offset),
+    )
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_old_question(self):
+        old_question = create_question("Old question", days_offset=-2)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        recent_question = create_question("Recent question", days_offset=-0.5)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+
+class IndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_with_one_question(self):
+        created_question = create_question("Test question", days_offset=-1)
+
+        response = self.client.get(reverse("polls:index"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test question")
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [created_question],
+            transform=lambda question: question,
+        )
